bd67f7e6a3893c4ac400ff0e53c97bad167fac30
OCircles <43825806+OCircles@users.noreply.github.com>
2019-01-25 22:05:44 +0100

    Fixed double MidiEvent add

--- MidiSilencer/Program.cs
+++ MidiSilencer/Program.cs
@@ -31,10 +31,7 @@ namespace ConsoleApp2
         private static void MakeSilent(string midifile)
         {
             var mf = new MidiFile(midifile, false);
-
-            var timeSignature = mf.Events[0].OfType<TimeSignatureEvent>().FirstOrDefault();
-
-
+            
             MidiEventCollection collection = new MidiEventCollection(mf.FileFormat, mf.DeltaTicksPerQuarterNote);
 
             for (int n = 0; n < mf.Tracks; n++)
@@ -42,14 +39,15 @@ namespace ConsoleApp2
                 collection.AddTrack();
                 foreach (var midiEvent in mf.Events[n])
                 {
-                    collection.AddEvent(midiEvent, n);
-
                     if (midiEvent.CommandCode == MidiCommandCode.NoteOn)
                     {
                         var ne = (NoteEvent)midiEvent;
                         ne.Velocity = 1;
                         collection.AddEvent((MidiEvent)ne, n);
 
+                    } else
+                    {
+                        collection.AddEvent(midiEvent, n);
                     }
                 }
             }
